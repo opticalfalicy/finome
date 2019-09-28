4e301b3d34f218005de5e81ce546bb7f9ea5d813
Unknown <opticalfalicy@gmail.com>
2019-09-28 08:05:16 -0400

    Structure and preliminary styling

---
+++ components/Bpm.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Bpm.sass';
+
+const Bpm = () => (
+  <div className="bpm-div">
+    <h1 className="bpm-text">BPM</h1>
+  </div>
+);
+
+export default Bpm;
---
+++ components/Button.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Button.sass';
+
+const Button = () => (
+  <div className="button-div">
+    <h1 className="button-text">Press To Start</h1>
+  </div>
+);
+
+export default Button;
