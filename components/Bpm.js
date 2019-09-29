68d97d3d3e4309dc2d5cc813fd4b21f322cbd3b9
Unknown <opticalfalicy@gmail.com>
2019-09-29 07:59:05 -0400

    introducing sound

--- components/Bpm.js
+++ components/Bpm.js
@@ -1,10 +1,36 @@
 import React from 'react';
 import './Bpm.sass';
 
-const Bpm = () => (
-  <div className="bpm-div">
-    <h1 className="bpm-text">BPM</h1>
-  </div>
-);
+export default class Bpm extends React.Component {
+  constructor (props) {
+    super (props);
+    this.state = {
+      bpm: Number,
+      dispBpm: 120,
+    };
+  }
 
-export default Bpm;
+  componentDidMount () {
+    this.setState ({
+      bpm: this.props.bpm,
+    });
+  }
+
+  getVal (val) {
+    dispVal = val;
+    val = 60000 / val;
+    this.setState ({
+      bpm: val,
+      dispBpm: dispVal,
+    });
+  }
+
+  render () {
+    let bpm = this.state.bpm;
+    return (
+      <div className="bpm-div">
+        <h1 className="bpm-text">{this.state.dispBpm}</h1>
+      </div>
+    );
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -5,15 +5,24 @@ import Bpm from '../components/Bpm';
 import './index.sass';
 
 class Main extends React.Component {
-  constructor () {
-    super ();
+  constructor (props) {
+    super (props);
+    this.handleBpmChange = this.handleBpmChange.bind (this);
+    this.state = {
+      counting: false,
+      bpm: 120,
+    };
+  }
+
+  handleBpmChange (bpm) {
+    this.setState ({bpm});
   }
 
   render () {
     return (
       <div className="main-div">
-        <Button />
-        <Bpm />
+        <Button counting={this.state.counting} bpm={this.props.bpm} />
+        <Bpm bpm={this.props.bpm} handleBpmChange={this.handleBpmChange} />
       </div>
     );
   }
